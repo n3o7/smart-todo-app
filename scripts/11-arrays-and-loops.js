8ff4a9e995c92c6e3f6b71a235ebef7905172dd5
n3o7 <stas.n3o7@gmail.com>
2026-03-29 11:35:33 +0200

    Added styles for filter buttons when button is active

--- scripts/11-arrays-and-loops.js
+++ scripts/11-arrays-and-loops.js
@@ -1,25 +1,28 @@
 const toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
 let currentFilter = 'all';
+const allButton = document.querySelector('.allFilterButton');
+const todayButton = document.querySelector('.todayFilterButton');
+const upcomingButton = document.querySelector('.upcomingFilterButton');
 renderTodoList();
  function addToDo(){
-      const taskToDo = document.querySelector('.final-todo-name');
-      const dateOfTask = document.querySelector('.js-date');
-      if (!taskToDo.value && !dateOfTask.value) {
-        alert('Enter task name and choose date');
-      } else if (!taskToDo.value) {
-        alert('Enter task name');
-      } else if (!dateOfTask.value) {
-        alert('Choose date');
-      }else{
-        toDoList.push({
-        name: taskToDo.value,
-        dueDate: dateOfTask.value
-        });
-        localStorage.setItem('toDoList', JSON.stringify(toDoList));
-        taskToDo.value = '';
-        dateOfTask.value='';
-        renderTodoList();
-      };
+  const taskToDo = document.querySelector('.final-todo-name');
+  const dateOfTask = document.querySelector('.js-date');
+  if (!taskToDo.value && !dateOfTask.value) {
+    alert('Enter task name and choose date');
+  } else if (!taskToDo.value) {
+    alert('Enter task name');
+  } else if (!dateOfTask.value) {
+    alert('Choose date');
+  }else{
+    toDoList.push({
+      name: taskToDo.value,
+      dueDate: dateOfTask.value
+     });
+     localStorage.setItem('toDoList', JSON.stringify(toDoList));
+     taskToDo.value = '';
+     dateOfTask.value='';
+     renderTodoList();
+    };
   };
    
     function renderTodoList(){
@@ -53,55 +56,26 @@ renderTodoList();
       localStorage.setItem('toDoList', JSON.stringify(toDoList));
       renderTodoList();
     }
-   
-
-  /*function renderFilteredList(list) {
-  let html = '';
-  for (let i = 0; i < list.length; i++) {
-    const todo = list[i];
-
-    html += `
-      <p>
-        ${todo.name} ${todo.dueDate}
-        <button onclick="
-      </p>
-    `;
-  };
-  document.querySelector('.final-todo-list').innerHTML = html;
-};
-  
-function filterUpcoming(){
-  const today = new Date().toISOString().split('T')[0];
-  const newArray=[];
-  for(let i=0;i<toDoList.length;i++){
-    if(toDoList[i].dueDate > today){
-      newArray.push(toDoList[i]);
-    }
-  }
-  renderFilteredList(newArray);
-}
- function filterToday(){
-      const newArray=[];
-      for(let i=0;i<toDoList.length;i++){
-        const today = new Date().toISOString().split('T')[0];
-        if(toDoList[i].dueDate === today){
-        newArray.push(toDoList[i]);
-      };
-    };
-    renderTodoList();
-  }
-*/
 function showAll() {
+  todayButton.classList.remove("active");
+  upcomingButton.classList.remove("active");
+  allButton.classList.add("active");
   currentFilter = 'all';
   renderTodoList();
 }
 
 function filterToday() {
+  allButton.classList.remove("active");
+  upcomingButton.classList.remove("active");
+  todayButton.classList.add("active");
   currentFilter = 'today';
   renderTodoList();
 }
 
 function filterUpcoming() {
+  allButton.classList.remove('active');
+  todayButton.classList.remove('active');
+  upcomingButton.classList.add('active');
   currentFilter = 'upcoming';
   renderTodoList();
 }
